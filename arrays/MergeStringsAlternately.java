ac45a5c1732d8c5153a34254ecf0a67d6a309df3
dwitiyabana <dwitiyabana07@gmail.com>
2026-06-12 00:57:40 +0530

    Add Merge Strings Alternately solution

---
+++ arrays/MergeStringsAlternately.java
@@ -0,0 +1,29 @@
+class MergeStringsAlternately {
+    public String MergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+
+        int i = 0, j = 0;
+
+        // Add characters alternately
+        while (i < word1.length() && j < word2.length()) {
+            result.append(word1.charAt(i));
+            result.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+
+        // Append remaining characters of word1
+        while (i < word1.length()) {
+            result.append(word1.charAt(i));
+            i++;
+        }
+
+        // Append remaining characters of word2
+        while (j < word2.length()) {
+            result.append(word2.charAt(j));
+            j++;
+        }
+
+        return result.toString();
+    }
+}
